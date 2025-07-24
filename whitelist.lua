c6246e60e5248910e62966d8b05e263aae92dc15
Moxiaobai <auroralittle@qq.com>
2025-07-24 09:01:48 +0800

    Update whitelist.lua

--- whitelist.lua
+++ whitelist.lua
@@ -1,11 +1,11 @@
 local whitelist = {
-    5032228877,
-    6160404733,
-    8941908059,
-    8967287543,
-    5362023738,
-    4241141996,
-    8634850315,
-    3188244200
+    [5032228877] = _G.Key .. "CdidByNEHubUserFirst",
+    [6160404733] = _G.Key .. "CdidByNEHubUserSecond",
+    [8941908059] = _G.Key .. "CdidByNEHubUserThird",
+    [8967287543] = _G.Key .. "CdidByNEHubUserFourth",
+    [5362023738] = _G.Key .. "CdidByNEHubUserFifth",
+    [4241141996] = _G.Key .. "CdidByNEHubUserSixth",
+    [8634850315] = _G.Key .. "CdidByNEHubUserSeventh",
+    [3188244200] = _G.Key .. "CdidByNEHubUserEighth"
 }
-return whitelist
+return
